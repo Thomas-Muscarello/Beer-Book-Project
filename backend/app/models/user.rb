f2f9a49ff045ce967172b0200606f58132e295df
Thomas-Muscarello <66980564+Thomas-Muscarello@users.noreply.github.com>
2021-04-20 12:38:24 -0400

    Update user.rb
    validates

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :beers, dependent: :destroy
+    
+    validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
